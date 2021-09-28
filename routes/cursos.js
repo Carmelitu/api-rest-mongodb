d7940856ee83ba21379e7d5dbe4ec339c7cbf7a1
Lucas Seijo <lucaseijo7@gmail.com>
2021-09-27 22:01:14 -0300

    se remueve joi de cursos

--- routes/cursos.js
+++ routes/cursos.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Curso = require('../models/curso_model');
-const Joi = require('joi');
 
 router.get('/', (req, res) => {
     let resultado = listarCursosActivos();
